9a4d3f5b16551e39713d31b831132ba1796e69f7
thanmai2903 <thanmaipalla329@gmail.com>
2025-06-09 21:35:56 +0530

    created db.js,updated admin.js,connected to mongodb custer and mongodb compass by adding url to the mongodb compass from mongodb cluster

---
+++ routes/db.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+console.log("Connected to ");
+mongoose.connect(
+  "mongodb+srv://thanmaipalla:adpvVjh12YqJDNnU@cluster0.tmku1mk.mongodb.net/coursera-app"
+);
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+const courseSchema = new Schema({
+  title: String,
+  discription: String,
+  price: Number,
+  imageUrl: String,
+  creatorId: ObjectId,
+});
+const purchaseSchema = new Schema({
+  userId: ObjectId,
+  courseId: ObjectId,
+});
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
+
+// adpvVjh12YqJDNnU;
